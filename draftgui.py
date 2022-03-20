5706308232c448daf4e0488c1375dd294fa1c30c
WhySoooSerious <haoyangcheng01@gmail.com>
2022-03-19 23:48:36 -0600

    added connection status indicator, modified connect and disconnect interface

--- draftgui.py
+++ draftgui.py
@@ -37,6 +37,7 @@ def openDebugTerminal():
     topWindow.title("Debug Terminal")
 
 def openBluetoothList():
+    global topWindow2
     topWindow2 = Toplevel()
     topWindow2.title("Discover Bluebooth Device...")
     global deviceList
@@ -48,12 +49,27 @@ def openBluetoothList():
     connectBlueButton.pack()
 
 def connectToBlueDevice():
-    messagebox.showinfo("Info", "Connected to "+ deviceList.get(ANCHOR))
+    global currentDeviceName
+    currentDeviceName = deviceList.get(ANCHOR)
+    messagebox.showinfo("Info", "Connected to " + currentDeviceName)
+    topWindow2.destroy()
+    connectionStatusBox.config(bg="green")
+    connectionStatusBox.delete(0, END)
+    connectionStatusBox.insert(0, "Connected to " + currentDeviceName)
+
+def disconToBlueDevice():
+    messagebox.showinfo("Info", "Disconnected from " + currentDeviceName)
+    connectionStatusBox.config(bg="red")
+    connectionStatusBox.delete(0, END)
+    connectionStatusBox.insert(0, "Not Connected")
 
 # Setup widegts
 # ---------------------------------------------------------------------------------------------
 # Device connection
 openBlueButton = Button(root, text = "Bluetooth", command=openBluetoothList)
+connectionStatusBox = Entry(root, width=20, borderwidth="5", fg="white", bg="red")
+connectionStatusBox.insert(0, "Not Connected")
+disconBlueButton = Button(root, text="Disconnect", command = disconToBlueDevice)
 
 # Dynamic data
 dataFrame = LabelFrame(root, text="Current Data", padx=20, pady=60)
@@ -92,5 +108,7 @@ temperatureLabel.grid(row=2, column=0)
 humidityLabel.grid(row=2, column=1)
 openTerminalButton.grid(row=4, column=0)
 openBlueButton.grid(row=5, column=0)
+connectionStatusBox.grid(row=6, column=0)
+disconBlueButton.grid(row=6, column=1)
 
 root.mainloop()
