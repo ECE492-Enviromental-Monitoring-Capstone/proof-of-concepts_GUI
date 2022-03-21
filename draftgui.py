15d4338192913247ce502d8dd0770b868bc206ef
WhySoooSerious <haoyangcheng01@gmail.com>
2022-03-21 14:24:55 -0600

    rearranged positions and added label image

--- draftgui.py
+++ draftgui.py
@@ -6,18 +6,20 @@
 from tkinter import *
 #from PIL import ImageTk, Image
 from tkinter import messagebox
+from tkinter import ttk
 #import bluetooth
 #nearby_devices = bluetooth.discover_devices()
 import threading
 import time
 import fakeParameters
 import datetime
+import os
 
 # Main Window
 root = Tk()
 root.title("AEMS panel")
 #root.iconbitmap('/Users/hoyeungching/Desktop/logo.ico')
-root.geometry("500x500")
+root.geometry("800x400")
 
 # ---[Function Definitions]------------------------------------------------------------------------------------
 def clickStartRec():
@@ -42,6 +44,9 @@ def clickStopRec():
 def openDebugTerminal():
     topWindow = Toplevel()
     topWindow.title("Debug Terminal")
+    topWindow.geometry("650x400")
+    textTerminal = Text(topWindow, width=80, height=20, fg="white", bg="black")
+    textTerminal.pack()
 
 def openBluetoothList():
     global topWindow2
@@ -93,7 +98,7 @@ def realTimeUpdateMeasurement():
     while True:
         # update temp
         temperatureBox.delete(0, END)
-        temperatureBox.insert(0, str(fakeParameters.getFakeTemp()) + " C")
+        temperatureBox.insert(0, str(fakeParameters.getFakeTemp()) + " ˚C")
 
         # update humi
         humidityBox.delete(0, END)
@@ -108,7 +113,7 @@ def realTimeUpdateMeasurement():
 
 # ---[Setup widegts]----------------------------------------------------------------------------------
 # Device connection
-openBlueButton = Button(root, text = "Bluetooth", command=openBluetoothList)
+openBlueButton = Button(root, text = "Discover", command=openBluetoothList)
 connectionStatusBox = Entry(root, width=20, borderwidth="5", fg="white", bg="red")
 connectionStatusBox.insert(0, "Not Connected")
 disconBlueButton = Button(root, text="Disconnect", command = disconToBlueDevice)
@@ -134,32 +139,51 @@ luminosityBox.insert(0, str(fakeParameters.getFakeLumi()) + " W")
 # USmic Rec
 statusBox = Entry(root, width=25,fg="white", bg="blue", borderwidth = "5")
 statusBox.insert(0, "Welcome to AEMS!")
-startRecButton = Button(root, text="Start Rec", padx=40, pady=20, command=clickStartRec, fg="green")
-stopRecButton = Button(root, text="Stop Rec", padx=40, pady=20, command=clickStopRec, fg="red")
+startRecButton = Button(root, text="Start Rec", padx=30, pady=15, command=clickStartRec, fg="green")
+stopRecButton = Button(root, text="Stop Rec", padx=30, pady=15, command=clickStopRec, fg="red")
+LoggingTimeLabel = Label(root, text = "Set Recording Time")
+loggingSecLabel = Label(root, text = "sec")
 LoggingSecBox = Entry(root, width=20, borderwidth="5")
+loggingMinLabel = Label(root, text="min")
 LoggingMinBox = Entry(root, width=20, borderwidth="5")
+loggingHourLabel = Label(root, text="hrs")
 LoggingHourBox = Entry(root, width=20, borderwidth="5")
 
 # Show debug terminal
 openTerminalButton = Button(root, text="Open Debug Terminal", command=openDebugTerminal)
 
+# picture
+pictureTextBox = Text(root, width = 90, height = 10)
+labelImage = PhotoImage(file="./label.png")
+pictureTextBox.image_create(END, image=labelImage)
+
 # ---[Positioning widgets]---------------------------------------------------------------------------------------
-startRecButton.grid(row=1, column=0)
-stopRecButton.grid(row=1, column=1)
-statusBox.grid(row=0, column=0, columnspan=2)
-temperatureBox.grid(row=3, column=0)
-humidityBox.grid(row=3, column=1)
-temperatureLabel.grid(row=2, column=0)
-humidityLabel.grid(row=2, column=1)
-openTerminalButton.grid(row=4, column=0)
-openBlueButton.grid(row=5, column=0)
-connectionStatusBox.grid(row=6, column=0)
-disconBlueButton.grid(row=6, column=1)
-luminosityLabel.grid(row=7, column=0)
-luminosityBox.grid(row=7, column=1)
-LoggingHourBox.grid(row=8, column=0)
-LoggingMinBox.grid(row=9, column=0)
-LoggingSecBox.grid(row=10, column=0)
+pictureTextBox.grid(row=0, column=1, columnspan=5)
+# connection part
+connectionStatusBox.grid(row=1, column=1)
+openBlueButton.grid(row=1, column=2)
+disconBlueButton.grid(row=1, column=3)
+openTerminalButton.grid(row = 1, column = 4)
+
+# rec part
+statusBox.grid(row=3, column=4, columnspan=1)
+LoggingTimeLabel.grid(row=4, column = 4)
+LoggingHourBox.grid(row=5, column = 4, columnspan=2)
+loggingHourLabel.grid(row=5, column = 6)
+LoggingMinBox.grid(row=6, column = 4, columnspan=2)
+loggingMinLabel.grid(row=6, column=6)
+LoggingSecBox.grid(row=7, column=4, columnspan=2)
+loggingSecLabel.grid(row=7, column=6)
+startRecButton.grid(row = 8, column = 4)
+stopRecButton.grid(row = 8, column = 5)
+
+# measurement part
+temperatureLabel.grid(row=3, column=1)
+temperatureBox.grid(row=3, column=2)
+humidityLabel.grid(row=4, column=1)
+humidityBox.grid(row=4, column=2)
+luminosityLabel.grid(row=5, column=1)
+luminosityBox.grid(row=5, column=2)
 
 # ---[Threading]--------------------------------------------------------------------------------------
 threading.Thread(target=realTimeUpdateMeasurement).start()
